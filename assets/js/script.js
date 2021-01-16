922bfba2570bed788c27d051838299845450a369
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-01-16 14:34:34 -0500

    updated social links

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.toggle').click(function(){
+			$('.toggle').toggleClass('active')
+			$('nav').toggleClass('active')
+		})
+	})
+	</script>
