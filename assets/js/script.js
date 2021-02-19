7b1c3faee4f21119db6eaabb14191e883cde3d94
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-02-19 10:09:34 -0500

    added faded background

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-//Social Bar
-<script type="text/javascript">
-	$(document).ready(function(){
-		$('.toggle').click(function(){
-			$('.toggle').toggleClass('active')
-			$('nav').toggleClass('active')
-		})
-	})
-	</script>
